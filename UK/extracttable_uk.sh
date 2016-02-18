79a10c48ff556790d6802fa5d6a434ffea7ef968
kepler <githubkepler.50s@gishpuppy.com>
2016-02-18 11:00:18 +0530

    moved htmls to ./html and tbls to ./tbl and reflecting programs

--- UK/extracttable_uk.sh
+++ UK/extracttable_uk.sh
@@ -14,10 +14,10 @@
 #        AUTHOR: YOUR NAME (), 
 #  ORGANIZATION: 
 #       CREATED: 01/27/2016 23:37
-#      REVISION:  2016-02-03 19:20
+#      REVISION:  2016-02-18 10:50
 #===============================================================================
 
-year=2016
+year=$( date +%Y )
 
 while [ $year -gt 1930 ] ; do
     url="${year}.html"
@@ -26,14 +26,14 @@ while [ $year -gt 1930 ] ; do
     # 1997.html and earlier don't have sortable
     # there are other wikitables too such as minor releases and box office and failures etc
     #sed -n '/<table class="wikitable/,/<\/table>/p' $url > "${year}.tbl"
-    sed -n '/<h2>.*Major_[Rr]eleases/,/<h2>/p' $url > "${year}.tbl"
-    if [[ ! -s "${year}.tbl" ]]; then
-        sed -n '/<table class="wikitable/,/<\/table>/p' $url > "${year}.tbl"
+    sed -n '/<h2>.*Major_[Rr]eleases/,/<h2>/p' $url > "tbl/${year}.tbl"
+    if [[ ! -s "tbl/${year}.tbl" ]]; then
+        sed -n '/<table class="wikitable/,/<\/table>/p' $url > "tbl/${year}.tbl"
     fi
     
-    wc -l "$year.tbl"
+    wc -l "tbl/$year.tbl"
     (( year-- ))
 done
 echo "Checking for movies with no URL"
-grep '^<td><i>[^<]' *.tbl
+grep '^<td><i>[^<]' tbl/*.tbl
 echo "If any rows appeared remove them from tbl files before running looptable.sh"
