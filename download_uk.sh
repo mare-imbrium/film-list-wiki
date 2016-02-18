296913be17bef46f45945e6bbc26011d7d0528c2
kepler <githubkepler.50s@gishpuppy.com>
2016-02-18 10:59:58 +0530

    moved htmls to ./html and tbls to ./tbl and reflecting programs

--- download_uk.sh
+++ download_uk.sh
@@ -14,17 +14,18 @@
 #        AUTHOR: YOUR NAME (), 
 #  ORGANIZATION: 
 #       CREATED: 01/27/2016 23:37
-#      REVISION:  2016-02-16 19:30
+#      REVISION:  2016-02-18 10:47
 #===============================================================================
 
-year=2016
+year=$( date +%Y)
+echo "Year=$year"
 host=https://en.wikipedia.org/wiki/List_of_British_films_of_
 mkdir UK
 cd UK
 while [ $year -gt 1930 ] ; do
     url="${host}${year}"
     echo $url
-    curl "$url" > ${year}.html
+    curl "$url" > html/${year}.html
     sleep 3
     (( year-- ))
 done
